6bdce7c2d5353511139618635001a5f529ce95dc
Allan Michael See <asee@mysupplychaingroup.com>
2026-06-28 23:34:17 +0000

    changed to this.byID instead of sap.ui.getcore

--- webapp/webapp/controller/Right.controller.js
+++ webapp/webapp/controller/Right.controller.js
@@ -832,18 +832,15 @@ sap.ui.define(
         i.fLoadingWeight = r.parseNumber(r.formatNumber(e.NetWeight, 2));
         i.sWeightUoM = e.WeightUoM;
         //add change 20260616 - set HU UoM when Pack HU is created
-        var huLWHUnit0 = sap.ui.core.Fragment.createId("pod---tabcontent--fragmentdefinition", "hu-lwh-unit0");
-        var huLWHUnit1 = sap.ui.core.Fragment.createId("pod---tabcontent--fragmentdefinition", "hu-lwh-unit1");
-        var huLWHUnit2 = sap.ui.core.Fragment.createId("pod---tabcontent--fragmentdefinition", "hu-lwh-unit2");			
-        //var oInpUnit0 = sap.ui.getCore().byId(huLWHUnit0);
-        //var oInpUnit1 = sap.ui.getCore().byId(huLWHUnit1);
-        //var oInpUnit2 = sap.ui.getCore().byId(huLWHUnit2);
-        //oInpUnit0.setText(e.UnitLwh);
-        //oInpUnit1.setText(e.UnitLwh);
-        //oInpUnit2.setText(e.UnitLwh);
-        huLWHUnit0.setText(e.UnitLwh);
-        huLWHUnit1.setText(e.UnitLwh);
-        huLWHUnit2.setText(e.UnitLwh);
+        var huLWHUnit0 = sap.ui.core.Fragment.createId(this.getTabId(a), "hu-lwh-unit0");
+        var huLWHUnit1 = sap.ui.core.Fragment.createId(this.getTabId(a), "hu-lwh-unit1");
+        var huLWHUnit2 = sap.ui.core.Fragment.createId(this.getTabId(a), "hu-lwh-unit2");			
+        var oInpUnit0 = this.byId(huLWHUnit0);
+        var oInpUnit1 = this.byId(huLWHUnit1);
+        var oInpUnit2 = this.byId(huLWHUnit2);
+        oInpUnit0.setText(e.UnitLwh);
+        oInpUnit1.setText(e.UnitLwh);
+        oInpUnit2.setText(e.UnitLwh);
         //end change 20260616 - set HU UoM when Pack HU is created        
         t.addShipHandlingUnit(a);
         this.setCurrentShipHandlingUnit(a);
